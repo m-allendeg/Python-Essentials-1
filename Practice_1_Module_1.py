ea8b1cb92adc6cf0c5afc39761374b44fdc1c9df
Marcos <garciamarcosallende@gmail.com>
2025-02-07 11:39:07 +0100

    Add files via upload

---
+++ Practice_1_Module_1.py
@@ -0,0 +1,14 @@
+print("Hisssssss...")
+"""In this program, I tried different things to break
+the code, such as removing quotes, parentheses... And
+I have realized that when an error occurs, it returns
+the traceback, the name of the error, its content, and
+its location."""
+
+
+
+
+
+
+
+
