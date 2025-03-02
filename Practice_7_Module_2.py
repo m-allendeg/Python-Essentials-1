d6586fbe304041d4b61ab10159a4bb0433f50bf5
Marcos <garciamarcosallende@gmail.com>
2025-03-02 20:20:18 +0100

    Add files via upload

---
+++ Practice_7_Module_2.py
@@ -0,0 +1,14 @@
+print(5+5)
+print(5-5)
+print(5*5)
+print(5/5)
+print(5//5)
+print(5&5)
+print(5**5)
+"""The character ** is an exp,
+* for multiplication, / for
+the division, // for integer
+division, % for the division
+with the rest.
+The Hierarchy is **,+ - unary,
+* / // &, + - binary."""
